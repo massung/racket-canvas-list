ee5b42858a80f73ecac9cbca5da2fc6499635049
Jeffrey Massung <massung@gmail.com>
2021-06-19 15:39:56 -0500

    Updated image.

--- info.rkt
+++ info.rkt
@@ -1,6 +1,9 @@
 #lang info
 
 (define collection "canvas-list")
+(define version "1.0.0")
+(define pkg-authors '("massung@gmail.com"))
+(define pkg-desc "Fast-rendering, single-selection, canvas control allowing custom drawing for a list of items.")
 (define deps '("base"))
 (define scribblings '(("scribblings/canvas-list.scrbl" ())))
-(define compile-omit-paths '("screenshots"))
+(define compile-omit-paths '("images"))
