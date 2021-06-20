2a1b7e5673dc8df06cc4ce023200f9902c373fd4
Jeffrey Massung <massung@gmail.com>
2021-06-19 23:21:18 -0500

    Fixed deps.

--- info.rkt
+++ info.rkt
@@ -4,6 +4,6 @@
 (define version "1.0.0")
 (define pkg-authors '("massung@gmail.com"))
 (define pkg-desc "Fast-rendering, single-selection, canvas control allowing custom drawing for a list of items.")
-(define deps '("base"))
+(define deps '("base" "draw-lib" "gui-lib"))
 (define scribblings '(("scribblings/canvas-list.scrbl" ())))
 (define compile-omit-paths '("images"))
